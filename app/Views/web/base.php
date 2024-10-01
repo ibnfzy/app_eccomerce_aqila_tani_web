0097671ff918b1cad318647c2f22679b3c9acd59
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2024-10-01 20:32:20 +0800

    menambahkan view website

---
+++ app/Views/web/base.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>EShopper - Bootstrap Shop Template</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="Free HTML Templates" name="keywords">
+  <meta content="Free HTML Templates" name="description">
+
+  <!-- Favicon -->
+  <link href="favicon.ico" rel="icon">
+
+  <!-- Google Web Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+    integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+    crossorigin='anonymous' />
+
+  <!-- Font Awesome -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css'
+    integrity='sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=='
+    crossorigin='anonymous' />
+
+  <!-- Libraries Stylesheet -->
+  <!-- Customized Bootstrap Stylesheet -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'
+    integrity='sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=='
+    crossorigin='anonymous' />
+  <link rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css'
+    integrity='sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=='
+    crossorigin='anonymous' />
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css'
+    integrity='sha512-6S2HWzVFxruDlZxI3sXOZZ4/eJ8AcxkQH1+JjSe/ONCEqR9L4Ysq5JdT5ipqtzU7WHalNwzwBv+iE51gNHJNqQ=='
+    crossorigin='anonymous' />
+  <link href="/web_assets/css/style.css" rel="stylesheet">
+
+  <style>
+  .owl-carousel .owl-stage {
+    display: flex !important;
+    align-items: center !important;
+    text-align: -webkit-center;
+  }
+
+  .owl-nav {
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    transform: translateY(-50%);
+  }
+
+  .owl-nav .owl-prev,
+  .owl-nav .owl-next {
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 10px;
+    border-radius: 50%;
+  }
+
+  .owl-nav .owl-prev {
+    position: absolute;
+    left: 20px;
+    /* Tambahkan margin kiri */
+  }
+
+  .owl-nav .owl-next {
+    position: absolute;
+    right: 40px;
+    /* Tambahkan margin kanan */
+  }
+
+  .owl-carousel .owl-item img {
+    width: 90%;
+    max-height: 500px;
+    object-fit: cover;
+    opacity: 0.7;
+  }
+  </style>
+</head>
+
+<body>
+  <?= $this->include('web/layouts/header'); ?>
+
+  <?= $this->renderSection('content'); ?>
+
+  <?= $this->include('web/layouts/footer'); ?>
+
+
+  <!-- Back to Top -->
+  <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+  <!-- JavaScript Libraries -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js'
+    integrity='sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=='
+    crossorigin='anonymous'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js'
+    integrity='sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg=='
+    crossorigin='anonymous'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js'
+    integrity='sha512-6sSYJqDreZRZGkJ3b+YfdhB3MzmuP9R7X1QZ6g5aIXhRvR1Y/N/P47jmnkENm7YL3oqsmI6AK+V6AD99uWDnIw=='
+    crossorigin='anonymous'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js'
+    integrity='sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ=='
+    crossorigin='anonymous'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'
+    integrity='sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=='
+    crossorigin='anonymous'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js'
+    integrity='sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ=='
+    crossorigin='anonymous'></script>
+
+  <!-- Contact Javascript File -->
+  <!-- <script src="/web_assets/mail/jqBootstrapValidation.min.js"></script>
+  <script src="/web_assets/mail/contact.js"></script> -->
+
+  <!-- Template Javascript -->
+  <!-- <script src="/web_assets/js/main.js"></script> -->
+
+  <?= $this->renderSection('script'); ?>
+
+  <script>
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 2,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa-solid fa-circle-chevron-left fa-xl text-success"></i>', // Ikon untuk tombol "prev"
+      '<i class="fa-solid fa-circle-chevron-right fa-xl text-success"></i>' // Ikon untuk tombol "next"
+    ]
+  })
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+  </script>
+
+  <?php
+  if (session()->getFlashdata('dataMessage')) {
+    foreach (session()->getFlashdata('dataMessage') as $item) {
+      echo '<script>toastr["' .
+        session()->getFlashdata('type-status') . '"]("' . $item . '")</script>';
+    }
+  }
+  if (session()->getFlashdata('message')) {
+    echo '<script>toastr["' .
+      session()->getFlashdata('type-status') . '"]("' . session()->getFlashdata('message') . '")</script>';
+  }
+  ?>
+</body>
+
+</html>
---
+++ app/Views/web/layouts/header.php
@@ -0,0 +1,59 @@
+<nav class="bg-success-subtle">
+  <!-- Topbar Start -->
+  <div class="container-fluid pt-5">
+    <div class="row align-items-center py-3 px-xl-5">
+      <div class="col-lg-3 d-none d-lg-block">
+        <a href="" class="text-decoration-none">
+          <h1 class="m-0 display-5 font-weight-semi-bold text-success">Toko Aqila Tani</h1>
+        </a>
+      </div>
+      <div class="col-lg-6 col-6 text-left">
+        <form action="">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Cari Barang">
+            <button class="btn btn-outline-success" type="button" id="button-addon2"><i
+                class="fa-solid fa-magnifying-glass"></i></button>
+          </div>
+        </form>
+      </div>
+      <div class="col-lg-3 col-6 text-right">
+        <a href="" class="btn btn-outline-success">
+          <i class="fa-solid fa-cart-shopping"></i>
+          0
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mb-5">
+    <nav class="navbar navbar-expand-lg bg-success-subtle navbar-light px-3">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Katalog Barang</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tentang Toko</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login Operator</a>
+            </li>
+          </ul>
+          <div class="navbar-nav ml-auto py-0">
+            <a href="" class="nav-item nav-link">Login</a>
+            <a href="" class="nav-item nav-link">Register</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!-- Navbar End -->
+</nav>
